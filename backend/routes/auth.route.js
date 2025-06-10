322e4e035844e4d940c642652082ea7c84ddf34c
Ayan Alam <ayanalam0025@gmail.com>
2025-06-10 13:02:08 +0530

    Initial backend setup

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,15 @@
+import express, { Router } from "express";
+import { forgotPassword, login, logout, resetPassword, signup, verifyEmail } from "../controllers/auth.controller.js";
+
+const router = Router();
+
+router.post("/signup",signup)
+router.post("/login", login)
+router.post("/logout",logout)
+
+router.post("/verify-email",verifyEmail)
+router.post("/forgot-password",forgotPassword)
+
+router.post("/reset-password",resetPassword)
+
+export default router;
