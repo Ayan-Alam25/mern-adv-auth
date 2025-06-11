3c4b4f9a79f1763c76d11348b7781bd19b38596d
Ayan Alam <ayanalam0025@gmail.com>
2025-06-11 12:53:27 +0530

    mailtrap and verify email added

---
+++ backend/mailtrap/emails.js
@@ -0,0 +1,53 @@
+import { mailtrapClient, sender } from "./mailtrap.config.js";
+import { VERIFICATION_EMAIL_TEMPLATE } from "./emailTemplates.js";
+
+export const sendVerificationEmail = async (email, verificationToken) => {
+  try {
+    if (!mailtrapClient || !sender) {
+      throw new Error("Mailtrap client or sender not initialized");
+    }
+
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: [{ email }],
+      subject: "Verify your email",
+      html: VERIFICATION_EMAIL_TEMPLATE.replace(
+        "{verificationCode}",
+        verificationToken
+      ),
+      category: "Email Verification",
+    });
+
+    console.log("✅ Email sent:", response);
+    return response;
+  } catch (error) {
+    console.error("❌ Mailtrap Error:", error.message);
+    throw new Error(`Failed to send email: ${error.message}`);
+  }
+};
+
+export const sendWelcomeEmail = async (email, name) => {
+  const recipient = [{email}];
+  try {
+    const response = await mailtrapClient.send({
+      from: sender,
+      to: recipient,
+      template_uuid: "f3c64b76-55ac-462e-8e6b-219f9dfbd51d",
+      template_variables:{
+        company_info_name: "Auth Company",
+        name: name,
+      }
+    });
+  } catch (error) {
+    console.error(`Error sending welcome email`, error);
+    throw new Error(`Error sending welcome email: ${error}`)
+  }
+}
+
+export const sendPasswordResetEmail = async (email, resetURL) => {
+
+}
+
+export const sendResetSuccessEmail = async(email) => {
+
+}
